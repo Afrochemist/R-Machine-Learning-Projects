e03beb61f8e3e49383a6a0caf79f036b6953d058
Afrochemist <afrochemist@gmail.com>
2018-04-06 23:49:33 -0500

    Ready for the next project

--- Kyphosis_Tree.R
+++ Kyphosis_Tree.R
@@ -1,2 +1,15 @@
 #neccessary libraries
 library(rpart)
+library(rpart.plot)
+#getting to know the data
+str(kyphosis)
+head(kyphosis)
+
+#now building a tree model
+tree <- rpart(Kyphosis ~ .,method='class', data=kyphosis)
+printcp(tree)
+plot(tree, uniform = T, main = "Kyphosis Tree")
+text(tree, use.n = T, all = T)
+
+#Now making the plot look nicer 
+prp(tree)
